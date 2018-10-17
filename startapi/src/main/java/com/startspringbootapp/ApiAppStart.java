09a01ec0d7b1a3db9dbb3e96a52a8d2f0c6411df
Manjusha.Dhamdhere <Manjusha.Dhamdhere@PD3020-847S022.OpusConsulting.com>
2018-10-17 17:19:05 +0530

    2 files by yk

---
+++ startapi/src/main/java/com/startspringbootapp/ApiAppStart.java
@@ -0,0 +1,14 @@
+package com.startspringbootapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAppStart {
+
+	public static void main(String[] args) {
+		//static method
+		SpringApplication.run(ApiAppStart.class, args);
+	}
+
+}
